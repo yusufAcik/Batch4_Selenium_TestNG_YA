123958c85a5773ff0697ec7e491668fe94ec7e42
yusufAcik <12yusufacik@gmail.com>
2023-07-11 23:14:19 +0300

    07 11

---
+++ src/test/java/com/krafttech/tests/day10_actions_fileUpload_isExecuted/JavaQuestion.java
@@ -0,0 +1,30 @@
+package com.krafttech.tests.day10_actions_fileUpload_isExecuted;
+
+public class JavaQuestion {
+    public static void main(String[] args) {
+
+        //Which method would you use to learn if a string is a rotation of another string?
+        //rotation of car->car, arc, rca
+        //sarı->sarı,arıs,rısa,ısar  -->en baştaki harfin en sona geçmesiyle oluşan yeni kelime..sıra bozulmuyor
+        //ve en fazla harf sayısı kadar oluyor
+
+        String str="yaman";
+        String str1="manya";
+
+
+    }
+    public static boolean isRotation(String str1,String str2){
+        if(str1.length()!=str2.length()) return false;
+
+        for (int i = 0; i < str1.length(); i++) {
+            str1=str1.substring(1)+str1.substring(0,1);
+            if(str1.equals(str2)) return true;
+        }
+        return false;
+    }
+
+    public static boolean isRotation1(String str1,String str2){
+        if(str1.length()!=str2.length()) return false;
+        return (str1+str1).contains(str2) ;
+    }
+}
